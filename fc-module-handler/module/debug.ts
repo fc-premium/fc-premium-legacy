9ec3405432b1d26779e91125da530f63ec5d549b
pytness <pytness@gmail.com>
2019-09-09 10:09:32 +0200

    Added todo function

--- fc-module-handler/module/debug.ts
+++ fc-module-handler/module/debug.ts
@@ -4,6 +4,22 @@ import { Module } from './module'
  * Implements an interface to the console api
  * @param module module reference from to get the configuration
  */
+
+/* // TODO: 
+	colors:
+
+	const clog = (a, b = "") => {
+	let c = "",
+		d = "";
+	"err" === b ? (c = "red", d = "[x]") :
+		"warn" === b ? (c = "darkorange", d = "[!]") :
+		"info" === b ? (c = "dodgerblue", d = "[i]") :
+		"ok" === b ? (c = "forestgreen", d = "[+]") :
+		c = "black";
+	console.log("%c" + d + " " + a, "color:" + c);
+};
+
+ */
 export class Debug {
 
 	private readonly hostModule: Module;
