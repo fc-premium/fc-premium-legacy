d62b6a959475c16f1d1ae20f3515e3d9241b1e09
pytness <pytness@gmail.com>
2019-09-09 10:10:13 +0200

    Added little ascii box function Used asciiBox to generate 'welcome' console message

--- fc-module-handler/init.ts
+++ fc-module-handler/init.ts
@@ -2,7 +2,28 @@ import { VERSION_HASH } from './definitions'
 import { ModuleHandler } from './module-handler'
 import { CONTROL_PANEL_MODULE } from './control-panel'
 
-console.log(`Version hash: ${VERSION_HASH}`);
+
+function asciiBox(lines: string[], minBoxWidth: number = 0): string {
+	let result: string = '';
+
+	const boxWidth: number = Math.max(minBoxWidth,
+		...lines.map(x => x.length + 2)
+	);
+
+	const header: string = '+'.padEnd(boxWidth, '=') + '+';
+
+	lines.forEach((line: string) => {
+		result += '|' + line.padEnd(boxWidth - 1, ' ') + '|\n'
+	});
+	return header + '\n' + result + header;
+};
+
+console.log(asciiBox([
+	` Title   : ${GM_info.script.name}`, '',
+	` Version : ${GM_info.script.version}`,
+	` Hash    : [${VERSION_HASH.slice(0, 8)}]`, '',
+	` By      : ${GM_info.script.author}`, '',
+], 40));
 
 // Load css resources
 (GM_info.script.resources).forEach((resource: GM_Resource) => {
